f936941217777ca12785d69e91063732d2ac64d9
R3yz0n <reyzon.thapa2020@gmail.com>
2024-01-06 20:09:52 +0545

    UI:Header and Footer Create a simple UI for header and Footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <Navbar expand="sm " bg='dark' variant='dark' style={{ width: '100vw' }} collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto ">
+                            <Nav.Link href="#home" className=' d-flex  align-items-center gap-2 '>
+                                <FaShoppingCart />
+                                Cart
+                            </Nav.Link>
+                            <Nav.Link href="#link" className='d-flex gap-2 align-items-center'>
+                                <FaUser />
+                                Sign In
+                            </Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Cart</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Sign In</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/src/products.js
@@ -0,0 +1,15 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods wireless bluetooth headphone",
+    image: "/image/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatile devices wirelessly Highqueryli auto offering ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
