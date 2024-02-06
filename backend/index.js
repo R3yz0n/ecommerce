7b80337f21f4b6dc05e3e4a766ed59de35929bad
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-06 19:09:58 +0545

    Add user routes to backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import connectToDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, routeNotFound } from "./middleware/errorHandler.js";
 import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 4000;
 const server = express();
+server.use(express.json());
+server.use(urlencoded({ extended: true }));
 const corsOptions = {
   origin: "http://localhost:3000",
   //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -16,6 +19,7 @@ const corsOptions = {
 server.use(cors(corsOptions));
 
 server.use("/api/products", productRoutes);
+server.use("/api/users", userRoutes);
 
 server.use(routeNotFound);
 server.use(errorHandler);
