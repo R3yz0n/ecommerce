b4cf1823a3996fd1c312f64e3eb747bada328c6c
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:21:04 +0545

    Add credentials option to baseQuery in apiSlice.js

--- frontend/src/store/slices/apiSlice.js
+++ frontend/src/store/slices/apiSlice.js
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../../constants/constants";
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: "include", // Set credentials to 'include'
+  }),
   tagTypes: ["Product", "User", "Order"],
   endpoints: (builder) => ({}),
 });
