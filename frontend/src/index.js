ca60d10adfcbb1f04bbaca6d6c54ea4b839a6c71
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:59:29 +0545

    Add new routes for Cart, Login, and Register screens

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,12 +14,18 @@ import {
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import store from "./store/store";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
