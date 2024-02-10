b9b2ba36eb63dda696de5bf7fc60f25d9e7c4c7c
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:58:40 +0545

    Add authMiddleware.js for token and admin verification

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import User from "../models/userModel.js";
+
+//Protected routes
+
+const tokenVerification = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.jwt;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+const adminVerification = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+export { tokenVerification, adminVerification };
