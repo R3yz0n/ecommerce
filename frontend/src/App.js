47c65ef5ec1b8d486ec84c64f8910c8c61565b66
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:59:20 +0545

    Add ToastContainer for displaying notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,31 @@ import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+
 import { getProductDetails, useGetProductDetailsQuery } from "./store/slices/productApiSlice";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(getProductDetails.initiate("65ae4511eb621001c7663c1e"));
+    // dispatch(getProductDetails.initiate("65ae4511eb621001c7663c1e"));
   }, [dispatch]);
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        // theme="dark"
+      />
       <Header />
       <main py="3">
         <Container>
