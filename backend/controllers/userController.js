8a33b97a48178f0f76788f374d56cee4e29d5dc6
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:58:35 +0545

    Update userController.js with authentication and user management functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,142 @@
-import asyncHandler from "../middleware/asyncHandler";
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
-// @desc   Register user
-// @route   POST /api/users/login
+import jwt from "jsonwebtoken";
+//  @desc   Register user
+// @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  } else {
+    const user = await User.create({ name, email, password });
+    if (user) {
+      generateToken(res, user._id);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  }
+
   res.send("auth users");
 });
 
 // @desc   Logout user / clear cookie
-// @route   POST /api/users/login
-// @access  Public
+// @route   POST /api/users/logout
+// @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("auth users");
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({ message: "Logged out sucessfully" });
 });
+
+//  @desc   Login user & get token
+// @route   Get /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+//  @desc   Get user profile
+// @route   Get /api/users
+// @access  Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get  users");
+});
+
+//  @desc   Get user profile
+// @route   Get /api/users/:id
+// @access  Private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get  user by id");
+});
+
+//  @desc   get user profile throught token
+// @route   put /api/users/
+// @access  Private/admin
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+//  @desc   update user profile throught token
+// @route   put /api/users/
+// @access  Private/admin
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+//  @desc   update user profile
+// @route   put /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("update  user ");
+});
+
+//  @desc   delete user profile
+// @route   put /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("delete  user profile");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  updateUserProfile,
+  getUserProfile,
+};
