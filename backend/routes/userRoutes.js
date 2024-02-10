45bc1510edc8f53e702c1ecf1ab77e8c59f53a4e
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-10 18:58:47 +0545

    Add user routes and controllers

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  authUser,
+  deleteUser,
+  getUserById,
+  getUserProfile,
+  getUsers,
+  logoutUser,
+  registerUser,
+  updateUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { adminVerification, tokenVerification } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(tokenVerification, adminVerification, getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router.route("/profile").get(tokenVerification, getUserProfile).put(updateUserProfile);
+router
+  .route("/:id")
+  .delete(deleteUser)
+  .get(getUserById)
+  .put(tokenVerification, adminVerification, updateUser);
+
+export default router;
