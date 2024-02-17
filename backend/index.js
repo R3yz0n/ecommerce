7580925d858a26af23b8e2b655c76a0348481a99
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-17 15:34:07 +0545

    Add session and authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -6,24 +6,33 @@ import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, routeNotFound } from "./middleware/errorHandler.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import session from "express-session";
+import passport from "passport";
+import "./middleware/passport.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 4000;
 const server = express();
-server.use(express.json());
-server.use(urlencoded({ extended: true }));
 
-// cookie parser middlware
-server.use(cookieParser());
 const corsOptions = {
   origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 server.use(cors(corsOptions));
+server.use(express.json());
+server.use(urlencoded({ extended: true }));
+
+// cookie parser middlware
+server.use(cookieParser());
+
+server.use(session({ secret: "helloboi", resave: true, saveUninitialized: true }));
+server.use(passport.initialize());
+server.use(passport.session());
 
 server.use("/api/products", productRoutes);
 server.use("/api/users", userRoutes);
+server.use("/api/auth", authRoutes);
 
 server.use(routeNotFound);
 server.use(errorHandler);
