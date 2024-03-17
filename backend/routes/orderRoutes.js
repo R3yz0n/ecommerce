631513be3cc1133876a5104b5b5b00faaa383f32
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-17 18:47:27 +0545

    Refactor orderRoutes.js to include getMyOrders route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {
-  createOrder,
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
   updateOrderToDelivered,
   getOrders,
+  getMyOrders,
 } from "../controllers/orderController.js";
 import { adminVerification, tokenVerification } from "../middleware/authMiddleware.js";
 
@@ -16,7 +16,8 @@ router
   .post(tokenVerification, addOrderItems)
   .get(tokenVerification, adminVerification, getOrders);
 
-router.route("/").get(tokenVerification, adminVerification, getOrderById);
+router.route("/mine").get(tokenVerification, getMyOrders);
+router.route("/:id").get(tokenVerification, getOrderById);
 
 router.route("/:id/pay").put(tokenVerification, updateOrderToPaid);
 
